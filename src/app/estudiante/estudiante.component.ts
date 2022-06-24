0121e5420ac58882f21cdb39d9246b824acb24b2
MARCO VARGAS <kmar98943@gmail.com>
2022-06-24 09:38:15 -0500

    first commit

---
+++ src/app/estudiante/estudiante.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { EstudianteService } from './estudiante.service';
+
+@Component({
+  selector: 'app-estudiante',
+  templateUrl: './estudiante.component.html',
+})
+export class EstudianteComponent implements OnInit {
+  EstudianteLista: any;
+
+
+  constructor(private estudianteService: EstudianteService) { }
+
+  ngOnInit(): void {
+    this.listaEstudiante01();
+  }
+
+  listaEstudiante01() {
+    this.estudianteService.ListaEstudiantes().subscribe((res:any) => {
+      this.EstudianteLista = res.data;
+    })
+}
+}
